03a8e0b2c25f463db609a9bfa97ef47d39a1958f
alexion11 <aionov78@yandex.ru>
2022-02-16 23:54:41 +0400

    Added user signup/signin using Firebase

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -1,15 +1,83 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { toast } from "react-toastify";
+import axios from "axios";
+import userService from "../services/user.service";
+import { setTokens } from "../services/locelStorage.service";
 
+const httpAuth = axios.create();
 const AuthContext = React.createContext();
 
 export const useAuth = () => {
     return useContext(AuthContext);
 };
 
-const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setUser] = useState({});
+    const [error, setError] = useState(null);
+    async function signUp({ email, password, ...rest }) {
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`;
+        try {
+            const { data } = await httpAuth.post(url, { email, password, returnSecureToken: true });
+            setTokens(data);
+            await createUser({ _id: data.localId, email, password, ...rest });
+            console.log("signUp auth", data);
+        } catch (error) {
+            errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            if (code === 400) {
+                if (message === "EMAIL_EXISTS") {
+                    const errorObject = {
+                        email: "This email already exists"
+                    };
+                    throw errorObject;
+                }
+            }
+        }
+    };
+
+    async function signIn({ email, password, ...rest }) {
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_KEY}`;
+        try {
+            const { data } = await httpAuth.post(url, { email, password, returnSecureToken: true });
+            // setTokens(data);
+            await createUser({ _id: data.localId, email, password, ...rest });
+            console.log("signIn auth", data);
+        } catch (error) {
+            errorCatcher(error);
+            const { code } = error.response.data.error;
+            if (code === 400) {
+                const errorObject = {
+                    email: "Incorrect email or password"
+                };
+                throw errorObject;
+            }
+            // throw new Error(error);
+        }
+    };
+
+    async function createUser(data) {
+        try {
+            const { content } = userService.create(data);
+            setUser(content);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    };
+
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{ signUp, signIn, currentUser }}>
             { children }
         </AuthContext.Provider>
     );
@@ -21,5 +89,3 @@ AuthProvider.propTypes = {
         PropTypes.node
     ])
 };
-
-export default AuthProvider;
